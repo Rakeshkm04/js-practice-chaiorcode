e208036b9711f6aa6621f21b0f670b2c30e51769
Rakesh Kumar <132670380+Rakeshkm04@users.noreply.github.com>
2024-08-21 11:22:17 +0000

    datatype conversion

--- Basic/app.js
+++ Basic/app.js
@@ -5,3 +5,23 @@ let location = null;
 
 
 console.table([name, age, isLoggedIn, location]);
+
+
+
+// conversion of datatype
+
+let score = 44;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
