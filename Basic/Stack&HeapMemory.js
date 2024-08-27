824cb0f56758044587c5c81120ea9f65c13bd6f0
Rakesh Kumar <132670380+Rakeshkm04@users.noreply.github.com>
2024-08-27 07:01:57 +0000

    Variable Memory Stored in Stack and Heap

---
+++ Basic/Stack&HeapMemory.js
@@ -0,0 +1,21 @@
+let myYoutubeName = "Rakesh Kumar";
+
+let anotherName = myYoutubeName;
+anotherName = "Chai or code";
+
+
+let userOne = {
+    name: "Rakesh Kumar",
+    upi: "user@ybl",
+    email: "rakesh@04"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "rakeshkm04@gmail.com";
+
+
+console.log(myYoutubeName);
+console.log(anotherName);
+console.log(userOne.email);
+console.log(userTwo.email);
