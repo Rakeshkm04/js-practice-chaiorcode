4a4ad833a63da70cac751fb08ea52cb057cee319
Rakesh Kumar <132670380+Rakeshkm04@users.noreply.github.com>
2024-09-21 06:33:43 +0000

    Variables both the Hitesh and the Piyush Goal

--- Basic/variables.js
+++ Basic/variables.js
@@ -27,3 +27,72 @@ Prefer not to use var
 Because of issue in block scope and functional scope.
 
 */
+
+
+
+
+// ---------Piyush Goal (Varibles Video = 2) -------------//
+
+// 1. DataType varName = value; = Strongly Typed Language.
+// 2. JS = Loosely Typed Language.
+
+
+
+//-------------------------//
+
+
+//Naming conv. 
+
+//firstname
+//first_name
+//FirstName
+//firstName (Most Pref.) Cammel Casing
+
+//---------------------------------//
+
+
+// Var -> Global Scope/
+
+
+if(true) {
+    var age = 22;
+}
+
+console.log(age);
+
+// Let -> Local Scope/
+
+if(true) {
+    let name = "Rakesh Kumar";
+    console.log(name);
+}
+
+
+// Const -> Local Scope/ --Value re-assign nhi ho sakti hai
+
+const name2 = "Piyush Goal";
+
+name2 = "Rakesh Kumar"; // not allowed in const variable
+
+console.log(name2);
+
+
+
+// Revise Var | Let | Const
+
+
+// Var -> Global Scope
+// Re-assign
+// Kahi bhi access Kr Sakte hai.
+
+
+// Let -> Local Scope
+// Re-assign
+// Access sirf Khud ke block ke ander hi kr sakte hai.  {}-> block 
+
+
+// Const -> Local Scope
+// Re-assign nhi kr sakte hai
+// Access sirf khud ke block ke ander hi kr sakte hai.
+
+// ==> Production mein almost Const use kiya jata hai.
